3d3249b577b60cd98d6b0bc17ecf9aa30db9a74f
AEU <aeuplayground@gmail.com>
2024-06-18 00:16:48 +0900

    20240618

--- src/app/_study/3_gingerbread.tsx
+++ src/app/_study/3_gingerbread.tsx
@@ -4,7 +4,18 @@ export default function Gingerbread() {
     return (
         <section className={styles.polygon_shape}>
             <svg width={200} height={200} viewBox="-100 -100 200 200">
-                <circle className={styles.head} cx={0} cy={-50} r={30} />
+                <circle className={styles.head} cx={0} cy={-50} r={30}  />
+
+                <circle className={styles.eye} cx={-12} cy={-55} r={3} />
+                <circle className={styles.eye} cx={12} cy={-55} r={3} />
+                <rect className={styles.mouth} x={-10} y={-40} width={20} height={5} rx={2} />
+
+                <line className={styles.limb} x1={-40} y1={-10} x2={40} y2={-10} />
+                <line className={styles.limb} x1={-25} y1={50} x2={0} y2={-15} />
+                <line className={styles.limb} x1={25} y1={50} x2={0} y2={-15} />
+
+                <circle className={styles.button} cx={0} cy={-10} r={5} />
+                <circle className={styles.button} cx={0} cy={10} r={5} />
             </svg>
         </section>
     );
---
+++ src/app/_study/4_house.tsx
@@ -0,0 +1,12 @@
+import styles from "@/app/_study/shape.module.css"
+
+
+export default function House() {
+    return(
+        <section className={styles.polygon_shape}>
+            <svg className={styles.house} width={200} height={200} viewBox="-100 -100 200 200" >
+                <polygon className={} />
+            </svg>
+        </section>    
+    );
+}
