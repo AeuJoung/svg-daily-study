187d872969cc360b5db9ef187c12827ed54ba7e5
AEU <aeuplayground@gmail.com>
2024-06-18 20:22:38 +0900

    20240618

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import BasicShape from "./_study/1_basic-shapes";
 import PolygonShape from "./_study/2_polygon";
 import Gingerbread from "./_study/3_gingerbread";
+import House from "./_study/4_house";
 
 //https://svg-tutorial.com/
 
@@ -13,6 +14,7 @@ export default function Home() {
           <BasicShape />
           <PolygonShape />
           <Gingerbread />
+          <House />
         </section>
     </main>
   );
